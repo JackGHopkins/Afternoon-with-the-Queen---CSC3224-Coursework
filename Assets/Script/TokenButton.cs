88115a95179cafef4ae73ea1f1ca579bb2f20bea
JackGHopkins <jackghopkins@gmail.com>
2021-02-22 18:08:34 +0000

    3x3 Grid complete. Added Check list.

--- Assets/Script/TokenButton.cs
+++ Assets/Script/TokenButton.cs
@@ -3,70 +3,57 @@ using System;
 
 public class TokenButton : TextureButton
 {
-    Texture textureNought = ResourceLoader.Load("res://Assets/Sprites/Nought3x3.png") as Texture;
     Texture textureCross = ResourceLoader.Load("res://Assets/Sprites/Cross3x3.png") as Texture;
-    Texture textureHover = ResourceLoader.Load("res://Assets/Sprites/Hover3x3.png") as Texture;
+    Texture textureCrossHover = ResourceLoader.Load("res://Assets/Sprites/CrossHovered3x3.png") as Texture;
+    Texture textureNought = ResourceLoader.Load("res://Assets/Sprites/Nought3x3.png") as Texture;
+    Texture textureNoughtHover = ResourceLoader.Load("res://Assets/Sprites/NoughtHovered3x3.png") as Texture;
+    Texture textureHover = ResourceLoader.Load("res://Assets/Sprites/Hover3x3.png") as Texture;    
 
-    NoughtsCrosses Grid;
-
+    int value = 0;
     [Export] int row = -1;
     [Export] int col = -1;
     [Export] private bool startFocused = false;
 
-    [Signal] delegate void CustomSignal(TextureButton button);
-    int turnCount = 0;
+    [Signal] delegate void CustomSignal(TokenButton button);
 
     public override void _Ready()
     {
         if (startFocused)
             GrabFocus();
-
         Reset();
-        //this.Connect("pressed", this, "OnPressed");
-    }
-
-    void OnPressed(){
-        SetToken(turnCount);
-        // Grid.SetTurnCount();
-        GD.Print("TurnCount is: ");
-        Disabled = true;
-    }
-
-    public void SetToken(int turnCount)
-    {
-        if (turnCount % 2 == 0)
-        {
-            SetCross();
-        }
-        else
-        {
-            SetNought();
-        }
-
     }
     public void SetCross()
     {
+        value = 1;
         TextureNormal = textureCross;
+        TextureHover = textureCrossHover;
     }
 
     public void SetNought()
     {
+        value = 10;
         TextureNormal = textureNought;
+        TextureHover = textureNoughtHover;
     }
 
     public void Reset()
     {
+        value = 0;
         TextureNormal = null;
         TextureHover = textureHover;
         FocusMode = FocusModeEnum.All;
     }
 
-        private void _on_Square_pressed(){
-        GD.Print(row + "," + col);
+    private void _on_Square_pressed()
+    {
         EmitSignal("CustomSignal", this);
     }
+    
+    public int GetValue() { return value; }
+    public int GetX() { return row;}
+    public int GetY() { return col;}
 
-    public int GetCol(){
-        return col;
-    }
+    public void SetValue(int value) { this.value = value;}
+
+    
 }
---
+++ README.md
@@ -0,0 +1,57 @@
+# CSC3024 - An Afternoon with the Queen
+
+This is **Jack Hopkins' CSC3024 Coursework**. The aim is to create a basic game for the coursework (aims will be further specified later). 
+
+## Premise
+
+In the game you take on the role of a prisoner which has been offered the chance of freedom if you please the Queen with a game of ***Noughts and Crosses***. However, what intially seems like a simple game gets made much harder as it is not specified what type of Noughts and crosses ***Her Majesty*** likes to play. 
+
+And so, you are put up against the queen to play a bizzare version of Noughts and Cross refered to as: ***Ultimate Noughts and Crosses***. 
+
+*Can you win?*
+*Can you gain you freedom again?*
+
+Only time will tell...
+
+# To Do list
+
+## Coursework 1 | March 1st 2021 |
+> This first piece of coursework provides an opportunity for you to develop a prototype of your game. A prototype is not a full implementation of a game but should allow a player to:
+> 1) Play a small or restricted element of a game.
+> 2) Understand the genre of a game.
+> 3) Realise the gameplay expectations.
+> 4) Achieve some limited goals within a game.
+> 5) Run the game from a web page (link must be provided).
+> 6) Provide developer help (like cheats to allow greater testing).
+
+## Game Design (10 Marks)
+**Possible Interaction** - If Player can influence the enviroment (3 Marks):
+- [ ] Place Tokens
+- [ ] AI / Queen reacts to placement of Token.
+- [ ] The Speed at which the Queen places the Token varies
+
+**Genre Clarity** - If its clear what the Player has to do after a little experimenting (3 Marks):
+- [ ] Make it clear its a game of Noughts and Crosses.
+
+**Goals** - What the Player can achieve (2 marks):
+- [ ] Player can win the Game
+
+**Ease of Play** - How intuative is it to play? (2 marks):
+- [ ] Make Graphics clear.
+
+## Technical Design (10 Marks)
+**Control Management** - Player can impact enviroment as expected (3 Marks):
+- [ ] Placing Token in correct places.
+- [ ] No dead spaces on Buttons.
+
+**Event Handling** - Enviroment reacto to player's interactions as expected (3 Marks):
+- [ ] Result of the Game is called correctly.
+
+**Enviroment Automation** Game plays even without player doing anything (4 Marks):
+- [ ] Implement Time limit for each opponent (Like that of Chess).
+- [ ] Faster you place Token the more time you get.
+
+## Developer's Tools (5 Marks)
+- [ ] Undo Turn button.
+- [ ] Reset button.
+- [ ] Infinite Time Option.
